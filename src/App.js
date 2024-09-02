7609cdda61dc7300f8a5e5c19b79c86d1de37f37
mrmuhib300@gmail.com <mrmuhib300@gmail.com>
2024-09-02 12:15:58 +0600

    100-Project-Excercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [noteTitle, setNoteTitle] = useState("");
+  const [notes, setNotes] = useState([
+    // { id: 1, title: "hello" },
+    // { id: 5, title: "name" },
+  ]);
+  const [editMode, setEditMode] = useState(false);
+  const [editableNode, setEditableNode] = useState(null);
+
+  const changeTitleHandler = (e) => {
+    setNoteTitle(e.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    // The trim() method removes whitespace from both sides of a string.
+    if (noteTitle.trim() === "") {
+      return alert("Please provete a title..");
+    }
+    editMode ? updateHandler() : createHandler();
+  };
+
+  const createHandler = () => {
+    const newNode = {
+      id: Date.now() + "",
+      title: noteTitle,
+    };
+
+    setNotes([newNode, ...notes]);
+    setNoteTitle("");
+  };
+
+  const reamoveHandler = (noteId) => {
+    const updateNode = notes.filter((note) => note.id !== noteId);
+    setNotes(updateNode);
+  };
+
+  const editHandler = (note) => {
+    setEditMode(true);
+    setEditableNode(note);
+    setNoteTitle(note.title);
+  };
+
+  const updateHandler = () => {
+    const updatalNodes = notes.map((item) => {
+      if (item.id === editableNode.id) {
+        return { ...item, title: noteTitle };
+      }
+      return item;
+    });
+    setNotes(updatalNodes);
+    setEditMode(false);
+    setNoteTitle("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submitHandler}>
+        <>
+          <input
+            className="input-fild"
+            placeholder="Enter Today List...."
+            tyep="text"
+            value={noteTitle}
+            onChange={changeTitleHandler}
+          ></input>
+          <button type="submit">{editMode ? "Update Node" : "Add Note"}</button>
+        </>
+      </form>
+      <div className="note-list">
+        <h2>Note List</h2>
+        {/* প্রথমে notes কে ধটি. তারপর ্এর ্ুউপর map() note name ekti function create kori  */}
+        <ul>
+          {notes.map((note) => (
+            <li key={note.id}>
+              <span>{note.title}</span>
+              <button onClick={() => editHandler(note)}>Edit</button>
+              <button onClick={() => reamoveHandler(note.id)}>Delate</button>
+              <hr></hr>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// const [] = useState();
